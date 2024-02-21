2e810e027caa0c46a7511bdcffe868dde0713259
Iaroslav Bratovnikov <hoshide@vk.com>
2024-02-21 05:37:18 +0300

    Initial commit

---
+++ src/components/CardList.vue
@@ -0,0 +1,24 @@
+<script setup>
+import AppCard from './AppCard.vue'
+
+defineProps({ items: Array })
+
+const emit = defineEmits(['addToFavorite', 'addToCart'])
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5" v-auto-animate>
+    <AppCard
+      v-for="item in items"
+      :key="item.id"
+      :id="item.id"
+      :title="item.title"
+      :image-url="item.imageUrl"
+      :price="item.price"
+      :isFavorite="item.isFavorite"
+      :isAdded="item.isAdded"
+      :onCLickFavorite="() => emit('addToFavorite', item)"
+      :onCLickAdd="() => emit('addToCart', item)"
+    />
+  </div>
+</template>
